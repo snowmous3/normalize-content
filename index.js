6e162e461d16e7f43a2459e53f29c60c479e6efd
Eduardo Neves <neves98@outlook.com>
2019-02-25 17:03:17 -0300

    Create methods

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
-exports.printMsg = function() {
-    console.log("Normalize Content instaled.");
-}
+module.exports = class NormalizeContent {
+    static lowerCase (data) {
+      return new Promise(async (resolve, reject) => {
+        try {
+          resolve(data.toLowerCase())
+        } catch (error) {
+          reject(error)
+        }
+      })
+    }
+    static removeAccents (data) {
+      return new Promise(async (resolve, reject) => {
+        try {
+          resolve(data.normalize('NFD').replace(/[\u0300-\u036f]/g, ''))
+        } catch (error) {
+          reject(error)
+        }
+      })
+    }
+  
+    static removeSpecialCaracters (data) {
+      return new Promise(async (resolve, reject) => {
+        try {
+          resolve(data.normalize('NFD').replace(/[^\w\s]|_/gi, ''))
+        } catch (error) {
+          reject(error)
+        }
+      })
+    }
+  
+    static clean (data) {
+      return new Promise(async (resolve, reject) => {
+        try {
+          data = await this.lowerCase(data)
+          data = await this.removeAccents(data)
+          resolve(await this.removeSpecialCaracters(data))
+        } catch (error) {
+          reject(error)
+        }
+      })
+    }
+  }
+  
